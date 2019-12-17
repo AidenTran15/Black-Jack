c2e7c1ee168cb6220a2dc64723fc5d482ede6463
AidenTran15 <aidenkiettran@gmail.com>
2019-12-17 18:48:33 +1100

    [Update create def hit]

--- casino.py
+++ casino.py
@@ -120,7 +120,67 @@ class player():
         self.cards = []
         self.curValue = 0
 
-    def deal
+    def deal(self):
+        r1 = random.randint(0, len(self.cardsL) - 1)
+        randCard = self.cards[r1]
+        self.cardsL.pop[r1]
+        self.cards.append(randCard)
+
+        r2 = random.randint(0, len(self.cardsL) - 1)
+        randCard2 = self.cardsL[r2]
+        self.cardsL.pop(r2)
+        self.cards.append(randCard2)
+
+        self.cardsL = self.cardsL
+
+        value = 0 
+        if self.cards[0][0] == 11 or self.cards[0][0] == 12 or self.cards[0][0] == 13:
+            value += 10
+        else:
+            value += self.cards[0][0]
+        if self.cards[1][0] == 11 or self.cards[1][0] == 12 or self.cards[1][0] == 13:
+            value += 10
+        else:
+            value += self.cards[1][0]
+
+        self.curValue = value
+            
+        if value == 11 and (self.cards[0][0] == 1 or self.cards[1][0] == 1):
+            self.curValue = 21
+
+
+        return self.cards
+
+    def hit(self):
+        value = 0 
+        rand = random.randint(1,len(self.cardsL))
+        card = self.cardsL[rand]
+        self.cards.append(card)
+        self.cardsL.pop(rand)
+
+        if card[0] == 11 or card[0] == 12 or card[0] == 13:
+            value = 10
+        elif card[0] == 1 and self.curValue == 10:
+            value == 11
+        elif card[0] == 1 and self.curValue < 11:
+            value = 11
+        elif card[0] == 1 and self.curValue == 20:
+            value = 1
+        else:
+            value = card[0]
+
+        self.curValue += value
+        if self.curValue > 21 and self.cards.__contains__(1):
+            if self.cards.count(1) == 1:
+                if self.curValue - 10 < 22:
+                    self.curValue = self.curValue - 10 
+
+
+        return card
+    
+    def  
+
+
 
 
 
