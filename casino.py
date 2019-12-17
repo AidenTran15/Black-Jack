d40a99df3180088fc1ed0fa06abc37c91f730dbd
AidenTran15 <aidenkiettran@gmail.com>
2019-12-17 15:59:42 +1100

    [AddNew] Create call card

--- casino.py
+++ casino.py
@@ -11,3 +11,21 @@ import random
  # D: Diamonds
  # C: Clubs
  # S: Spades
+
+
+class cards:
+     def __init__(self):
+         self.cardsL = []
+         for x in range(1,14):
+             card = x
+             for i in range(4):
+                if i == 0:
+                    suit = 'H'
+                elif i == 1:
+                    suit = 'D'
+                elif i == 2:
+                    suit = 'C'
+                elif i == 3:
+                    suit = 'S'
+                self.cardsL.append([card, suit])
+
